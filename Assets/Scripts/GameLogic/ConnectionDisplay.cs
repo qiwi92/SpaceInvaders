c188b6aaa7479d63c6e0f41327d420d5724d0a6c
Josh <xell.art@hotmail.com>
2018-09-28 12:45:57 +0200

    Added Fake Iaps and Tweaked UI

---
+++ Assets/Scripts/GameLogic/ConnectionDisplay.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+
+namespace GameLogic
+{
+    public class ConnectionDisplay : MonoBehaviour
+    {
+        [SerializeField] private GameObject _connecting;
+        [SerializeField] private GameObject _connectionFailed;
+        [SerializeField] private GameObject _loadingAnimation;
+
+        private void Start()
+        {
+            _loadingAnimation.SetActive(true);
+            _connecting.SetActive(false);
+            _connectionFailed.SetActive(false);
+        }
+
+        private void OnEnable()
+        {
+            StopCoroutine(Blyat());
+            _loadingAnimation.SetActive(true);
+            _connecting.SetActive(false);
+            _connectionFailed.SetActive(false);
+
+            StartCoroutine(Blyat());
+            
+        }
+
+        private IEnumerator Blyat()
+        {
+            yield return new WaitForSeconds(0.5f);
+            _connecting.SetActive(true);
+
+            yield return new WaitForSeconds(4f);
+            _connectionFailed.SetActive(true);
+            _loadingAnimation.SetActive(false);
+
+        }
+    }
+}
